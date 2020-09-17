dcfa916546b4b654e3e8f5ed4f078c087e9bd364
Tamara Bartlett <tamara.bartlett@uplight.com>
2020-09-17 09:19:47 -0600

    Adds platypus spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './platypus.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/Platypus"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn About Platypus
         </a>
       </header>
     </div>
